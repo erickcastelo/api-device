e759e61c3db64169d020570fe5e5d7fc75787441
Erick Castelo Branco <erickcastelo2@gmail.com>
2021-06-12 19:48:59 -0400

    Add env

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
   dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: 'root',
-  database: 'device',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB,
   define: {
     timestamps: true,
     underscored: true
